f4deaae35f374f314cd90c625efb45d802f2e290
fariafreelancingwali123 <fariasammer@gmail.com>
2025-02-08 12:32:15 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Payment Platform</title>
+</head>
+<body>
+    <h1>Welcome to Our Payment Platform</h1>
+    <a href="signup.php">Sign Up</a>
+    <a href="login.php">Login</a>
+</body>
+</html>
